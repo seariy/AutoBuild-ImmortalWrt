fdad8789bbaf87127beb2b1d3de2c5339528535f
Seariy <hailangzhisheng@gmail.com>
2025-01-22 00:09:08 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -6,7 +6,7 @@ echo "编译固件大小为: $PROFILE MB"
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 
 # 克隆SSR-PLUS+源码
-git clone --depth=1 https://github.com/fw876/helloworld
+clone_all https://github.com/fw876/helloworld
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 
 # 定义所需安装的包列表
